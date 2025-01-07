9ba707c3501b064b9f708f4a59b802c26dd11ee0
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2025-01-07 10:35:50 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,47 @@
+function _2G () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(392, music.beat(BeatFraction.Double))
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
+    EDCD()
+    _2E()
     music.playTone(330, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Quarter))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
+    _2D()
     music.playTone(294, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Quarter))
     music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Double))
+    _2G()
     music.rest(music.beat(BeatFraction.Quarter))
+    EDCD()
+    _3E()
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    _2D()
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Breve))
+})
+function _2E () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+}
+function EDCD () {
     music.playTone(330, music.beat(BeatFraction.Whole))
     music.playTone(294, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Quarter))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Breve))
-    basic.showString("TaDaaaa")
-})
+}
+function _3E () {
+    for (let index = 0; index < 3; index++) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+}
+function _2D () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(294, music.beat(BeatFraction.Whole))
+    }
+}
